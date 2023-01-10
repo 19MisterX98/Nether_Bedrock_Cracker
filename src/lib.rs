d90a10d8e45ea13112961f69cdf9ddeeb1e58bba
misterx <weinboy11@gmail.com>
2023-01-10 19:45:55 +0100

    works

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-const MASK48: u64 = (1<<48)-1;
-const MULT: u64 = 25214903917;
+use java_random::{JAVA_LCG, Random};
+use next_long_reverser::get_next_long;
+
+const MASK48: u64 = 0xffff_ffff_ffff;
 
 pub enum BlockType {
     BEDROCK,
@@ -7,27 +9,27 @@ pub enum BlockType {
 }
 
 pub struct Block {
-    //store a lambda that checks
-    pub pos_hash: u64,
-    pub lower_bound: u64,
-    pub upper_bound: u64,
-    pub valid_range: u64,
+    pos_hash: u64,
+    lower_bound: u64,
+    upper_bound: u64,
+    valid_range: u64,
     possible_range: u64,
 }
 
 impl Block {
 
-    fn to_check(&self, lower_bits: u64) -> CheckObject {
+    fn create_check(&mut self, lower_bits: u64) -> CheckObject {
         let lower_bits_mask = (1 << lower_bits) - 1;
+        self.check_with_bits(lower_bits);
         CheckObject::new(self.pos_hash, self.lower_bound, self.upper_bound, lower_bits_mask)
     }
 
     /*
     Figure out how many seeds an operation filters
      */
-    pub fn discarded_seeds(&self, lower_bits: u64) -> f64 {
+    fn discarded_seeds(&self, lower_bits: u64) -> f64 {
         let lower_bits_mask = (1 << lower_bits) - 1;
-        let bound = self.bound() + lower_bits_mask * MULT;
+        let bound = self.bound() + lower_bits_mask * JAVA_LCG.multiplier;
         let success_chance = bound as f64 / self.possible_range as f64;
         let fail_chance = 1.0 - success_chance;
         if fail_chance <= 0.0 {
@@ -37,13 +39,12 @@ impl Block {
     }
 
     //the chance for new info decreases
-    pub fn check_with_bits(&mut self, lower_bits: u64, checks: &mut Vec<CheckObject>) {
+    fn check_with_bits(&mut self, lower_bits: u64) {
         let lower_bits_mask = (1 << lower_bits) - 1;
-        let jiggle_room = lower_bits_mask * MULT;
+        let jiggle_room = lower_bits_mask * JAVA_LCG.multiplier;
         let new_range = jiggle_room + self.bound();
         assert!(new_range < self.possible_range);
         self.possible_range = new_range;
-        checks.push(self.to_check(lower_bits));
     }
 
     fn bound(&self) -> u64 {
@@ -53,9 +54,10 @@ impl Block {
 
     pub fn new(x: i32, y: i32, z: i32, block_type: BlockType) -> Block {
 
-        let pos_hash = Block::hashcode(x,y,z);
+        let pos_hash = Block::hashcode(x,y,z) ^ JAVA_LCG.multiplier;
         let (lower_bound, upper_bound) = Block::bounds(y, block_type);
         let valid_range = upper_bound - lower_bound;
+        println!("Block: {x}, {y}, {z}, {pos_hash}");
 
         Block { pos_hash, lower_bound, upper_bound, valid_range, possible_range: MASK48 }
     }
@@ -64,7 +66,7 @@ impl Block {
         let mut pos_hash = (x.wrapping_mul(3129871)) as i64 ^ ((z as i64).wrapping_mul(116129781)) ^ y as i64;
         pos_hash = pos_hash.wrapping_mul(pos_hash).wrapping_mul(42317861).wrapping_add(pos_hash.wrapping_mul(11));
         let pos_hash = pos_hash as u64;
-        (pos_hash >> 16)
+        pos_hash >> 16
     }
 
     fn bounds(mut layer: i32, block_type: BlockType) -> (u64, u64) {
@@ -94,6 +96,36 @@ impl Block {
     }
 }
 
+pub fn create_filter_tree(blocks: &mut Vec<Block>) -> Layer {
+    //sort everything by filter power
+    //wanted to try functional programming
+    let layers: Vec<Layer> = (0..=12)
+        .rev()
+        .map(|bits| {
+            let mut checks = blocks.iter_mut()
+                .map(|block| (block.discarded_seeds(bits), block))
+                .filter(|(discarded_seeds, _)| *discarded_seeds > 0.0)
+                .collect::<Vec<(f64, &mut Block)>>();
+
+            checks.sort_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap());
+
+            let res = checks.into_iter()
+                .map(|(_ ,a)| a.create_check(bits))
+                .collect();
+
+            Layer::new(bits, res)
+        })
+        .collect();
+    layers.into_iter()
+        .rev()
+        .reduce(|next_layer, mut layer| {
+            let prev_layer = Box::new(next_layer);
+            layer.next_layer = Some(prev_layer);
+            layer
+        })
+        .expect("For some reason no layers were created")
+}
+
 #[derive(Debug)]
 pub struct CheckObject {
     pos_hash: u64,
@@ -104,12 +136,152 @@ pub struct CheckObject {
 impl CheckObject {
 
     fn check(&self, upper_bits: u64) -> bool {
-        (upper_bits ^ self.pos_hash).wrapping_mul(MULT).wrapping_add(self.offset) & MASK48 > self.condition
+        ((upper_bits ^ self.pos_hash).wrapping_mul(JAVA_LCG.multiplier).wrapping_add(self.offset) & MASK48) < self.condition
     }
 
     pub fn new(pos_hash: u64, lower_bound: u64, upper_bound: u64, lower_bit_mask: u64) -> CheckObject {
         let offset = MASK48 - upper_bound;
-        let condition = lower_bound.wrapping_add(offset).wrapping_sub(lower_bit_mask * MULT);
+        let pos_hash = pos_hash & (MASK48 - lower_bit_mask);
+        let condition = lower_bound.wrapping_add(offset).wrapping_sub(lower_bit_mask * JAVA_LCG.multiplier);
         CheckObject { pos_hash, condition, offset }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Layer {
+    pub checks: Vec<CheckObject>,
+    pub split: u64,
+    pub next_layer: Option<Box<Layer>>,
+}
+
+impl Layer {
+    pub fn new(lower_bits: u64, checks: Vec<CheckObject>) -> Layer {
+        let split: u64 = 1 << (lower_bits.saturating_sub(1));
+        Layer {checks, split, next_layer: None}
+    }
+
+    pub fn run_checks(&self, upper_bits: u64) {
+        //println!("run:");
+        for check in self.checks.iter() {
+            if check.check(upper_bits) {
+                //println!("check failed: {upper_bits} {:#?}",check);
+                return;
+            }
+            //println!("success");
+        }
+        match self.next_layer.as_ref() {
+            Some(layer) => {
+                layer.run_checks(upper_bits);
+                layer.run_checks(upper_bits + self.split);
+            },
+            None => {
+                let chance = upper_bits as f64 / MASK48 as f64;
+                println!("Found Seed: {} percent: {}", upper_bits, chance)
+            },
+        }
+    }
+}
+
+fn recover_world_seeds(seed: u64, is_floor: bool) -> Vec<u64> {
+    let mut hashcode = 343340730; //roof
+    if is_floor {
+        hashcode = 2042456806; //floor
+    }
+
+    get_next_long(seed).into_iter()
+        .flat_map(|mut seed| {
+            seed ^= JAVA_LCG.multiplier;
+            seed ^= hashcode;
+            get_next_long(seed).into_iter()
+        })
+        .map(|mut seed| {
+            seed ^= JAVA_LCG.multiplier;
+            seed & MASK48
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::BlockType::BEDROCK;
+    use super::*;
+
+/*
+upper_bits: 81699833426244
+lower_bits: 29905
+seed: 5354280283422356689
+floor: 9210758467792927021
+roof: 1442567685227760047
+*/
+
+    #[test]
+    fn test_hashcode() {
+        let block = Block::new(-98, 4, -469, BEDROCK);
+        assert_eq!(block.pos_hash, 99261249361405)
+    }
+
+    #[test]
+    fn test_seed_conversion() {
+        let roof_seed = 1442567685227760047 & MASK48;
+        let seed = 5354280283422356689 & MASK48;
+        assert!(recover_world_seeds(roof_seed, false).contains(&seed));
+    }
+
+    #[test]
+    fn test_world_seed_to_roof() {
+        let seed = 5354280283422356689 & MASK48;
+        let roof_seed = 1442567685227760047 & MASK48;
+
+        let mut rand = Random::with_seed(seed);
+        let mut lon = rand.next_long() as u64& MASK48;
+        lon = lon ^ 343340730;
+        rand.set_seed(lon);
+        lon = rand.next_long() as u64 & MASK48;
+
+        assert_eq!(lon, roof_seed);
+    }
+
+    #[test]
+    fn test_bedrock_matches() {
+        let mut seed = (9210758467792927021 ^ JAVA_LCG.multiplier) & MASK48;
+        println!("{seed}");
+        seed = seed & 0xFFFF_FFFF_F000;
+        let mut blocks = Vec::new();
+        blocks.push(Block::new(-98, 4, -469, BEDROCK));
+        blocks.push(Block::new(-101, 4, -465, BEDROCK));
+        blocks.push(Block::new(-101, 4, -463, BEDROCK));
+        blocks.push(Block::new(-101, 4, -457, BEDROCK));
+        blocks.push(Block::new(-101, 4, -453, BEDROCK));
+        blocks.push(Block::new(-100, 4, -456, BEDROCK));
+        blocks.push(Block::new(-100, 4, -449, BEDROCK));
+        blocks.push(Block::new(-99, 4, -464, BEDROCK));
+        blocks.push(Block::new(-99, 4, -459, BEDROCK));
+        blocks.push(Block::new(-99, 4, -455, BEDROCK));
+        blocks.push(Block::new(-98, 4, -461, BEDROCK));
+        blocks.push(Block::new(-98, 4, -460, BEDROCK));
+        blocks.push(Block::new(-96, 4, -467, BEDROCK));
+        blocks.push(Block::new(-96, 4, -465, BEDROCK));
+        blocks.push(Block::new(-96, 4, -464, BEDROCK));
+        blocks.push(Block::new(-96, 4, -452, BEDROCK));
+        blocks.push(Block::new(-95, 4, -465, BEDROCK));
+        blocks.push(Block::new(-95, 4, -458, BEDROCK));
+        blocks.push(Block::new(-95, 4, -449, BEDROCK));
+        blocks.push(Block::new(-94, 4, -462, BEDROCK));
+        blocks.push(Block::new(-94, 4, -459, BEDROCK));
+        blocks.push(Block::new(-94, 4, -454, BEDROCK));
+        blocks.push(Block::new(-93, 4, -467, BEDROCK));
+        blocks.push(Block::new(-93, 4, -465, BEDROCK));
+        blocks.push(Block::new(-93, 4, -463, BEDROCK));
+        blocks.push(Block::new(-93, 4, -455, BEDROCK));
+        blocks.push(Block::new(-92, 4, -468, BEDROCK));
+        blocks.push(Block::new(-92, 4, -467, BEDROCK));
+
+        blocks.iter_mut()
+            .map(|block| block.create_check(12))
+            .filter(|check| check.check(seed))
+            .for_each(|check| {
+                panic!("check failed: {:#?}", check)
+            })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,51 @@
 extern crate core;
+
 use std::time::SystemTime;
-use bedrock_cracker::{Block, CheckObject};
+use java_random::{JAVA_LCG, Random};
+use next_long_reverser::get_next_long;
+use bedrock_cracker::{Block, create_filter_tree, Layer};
 use bedrock_cracker::BlockType::{BEDROCK, OTHER};
 
-const MASK48: u64 = (1<<48)-1;
-const MULT: u64 = 25214903917;
-
 fn main() {
-    println!("Hello, world!");
+    let start = SystemTime::now();
 
     let mut blocks = Vec::new();
 
-    blocks.push(Block::new(1,4,0,BEDROCK));
-    blocks.push(Block::new(1,3,1,BEDROCK));
-    blocks.push(Block::new(1,2,2,BEDROCK));
-    blocks.push(Block::new(1,1,3,BEDROCK));
-    blocks.push(Block::new(1,4,4,BEDROCK));
-    blocks.push(Block::new(1,3,5,BEDROCK));
-    blocks.push(Block::new(1,2,6,BEDROCK));
-    blocks.push(Block::new(1,1,7,BEDROCK));
-    blocks.push(Block::new(1,4,8,OTHER));
-    blocks.push(Block::new(1,3,9,OTHER));
-    blocks.push(Block::new(1,2,10,OTHER));
-    blocks.push(Block::new(1,1,11,OTHER));
-    blocks.push(Block::new(1,4,12,OTHER));
-    blocks.push(Block::new(1,3,13,OTHER));
-    blocks.push(Block::new(1,2,14,OTHER));
-    blocks.push(Block::new(1,1,15,OTHER));
-    blocks.push(Block::new(1,4,16,BEDROCK));
-    blocks.push(Block::new(1,4,17,BEDROCK));
+    blocks.push(Block::new(-98, 4, -469, BEDROCK));
+    blocks.push(Block::new(-101, 4, -465, BEDROCK));
+    blocks.push(Block::new(-101, 4, -463, BEDROCK));
+    blocks.push(Block::new(-101, 4, -457, BEDROCK));
+    blocks.push(Block::new(-101, 4, -453, BEDROCK));
+    blocks.push(Block::new(-100, 4, -456, BEDROCK));
+    blocks.push(Block::new(-100, 4, -449, BEDROCK));
+    blocks.push(Block::new(-99, 4, -464, BEDROCK));
+    blocks.push(Block::new(-99, 4, -459, BEDROCK));
+    blocks.push(Block::new(-99, 4, -455, BEDROCK));
+    blocks.push(Block::new(-98, 4, -461, BEDROCK));
+    blocks.push(Block::new(-98, 4, -460, BEDROCK));
+    blocks.push(Block::new(-96, 4, -467, BEDROCK));
+    blocks.push(Block::new(-96, 4, -465, BEDROCK));
+    blocks.push(Block::new(-96, 4, -464, BEDROCK));
+    blocks.push(Block::new(-96, 4, -452, BEDROCK));
+    blocks.push(Block::new(-95, 4, -465, BEDROCK));
+    blocks.push(Block::new(-95, 4, -458, BEDROCK));
+    blocks.push(Block::new(-95, 4, -449, BEDROCK));
+    blocks.push(Block::new(-94, 4, -462, BEDROCK));
+    blocks.push(Block::new(-94, 4, -459, BEDROCK));
+    blocks.push(Block::new(-94, 4, -454, BEDROCK));
+    blocks.push(Block::new(-93, 4, -467, BEDROCK));
+    blocks.push(Block::new(-93, 4, -465, BEDROCK));
+    blocks.push(Block::new(-93, 4, -463, BEDROCK));
+    blocks.push(Block::new(-93, 4, -455, BEDROCK));
+    blocks.push(Block::new(-92, 4, -468, BEDROCK));
+    blocks.push(Block::new(-92, 4, -467, BEDROCK));
 
-    let _floor_seed = compute_floor_seeds(&mut blocks, (1<<12)-1);
+    let checks = create_filter_tree(&mut blocks);
 
-}
-
-fn sorting_blocks(blocks: &mut Vec<Block>) -> Vec<CheckObject> {
-    let mut checks: Vec<CheckObject> = Vec::new();
-    //sort everything by filterpower
-    loop {
-        let max_filter_power = blocks.iter()
-            .map(|block| {
-                (0..=12)
-                    .map(|bits| block.discarded_seeds(bits))
-                    .reduce(f64::max)
-                    .unwrap()
-            })
-            .reduce(f64::max)
-            .unwrap();
-        if max_filter_power == 0.0 {
-            break;
-        }
-        blocks.iter_mut().for_each(|mut block| {
-            (0..=12)
-                .for_each(|num| {
-                    if block.discarded_seeds(num) == max_filter_power {
-                        println!("Filter power: {}, {}", max_filter_power, num);
-                        block.check_with_bits(num, &mut checks);
-                    }
-                })
-            });
-    }
-    checks
-}
-
-
-fn compute_floor_seeds(blocks: &mut Vec<Block>, lower_bits_mask: u64) -> Vec<u64> {
-    let checks = sorting_blocks(blocks);
-
-    let mut output = Vec::new();
-    let start = SystemTime::now();
-
-    for check in checks.iter() {
-        //println!("{:#?}", check);
+    for upper_bits in (0..0xFFFF_FFFF_FFFF).step_by(1<<12) {
+        checks.run_checks(upper_bits);
     }
 
-    println!("length: {}",checks.len());
-
-    /*
-    2. group blocks by information/layer
-    3. figure out which layer to check and with how many bits to do that.
-    4. create lambdas and chain them?
-
-    //roof_seed: ("minecraft:bedrock_roof".hash ^ mult ^ seed)*mult+addend
-    //floor_seed: ("minecraft:bedrock_floor".hash ^ mult ^ seed)*mult+addend
-     */
-    panic!();
-
-    'outer: for upper_bits in (0..MASK48).step_by(lower_bits_mask as usize + 1) {
-
-        for block in blocks.iter() {
-            if block.upper_bound == 0 {
-                continue 'outer;
-            }
-        }
-
-
-        let now = SystemTime::now().duration_since(start).unwrap().as_secs();
-        let chance = upper_bits as f64 / (1i64<<48) as f64;
-        println!("{chance}, {now}");
-
-        output.push(upper_bits);
-    }
-
-    output
+    let delta = SystemTime::now().duration_since(start).expect("Couldnt stop time");
+    println!("Time elapsed: {}", delta.as_secs());
 }
-
-
-//todo the addition should be respected aswell
-
